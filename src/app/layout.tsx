f9d122c9092e68afe1b716ee24b5fa5a698a646a
Sebastian Mendoza <sebmendozaa@gmail.com>
2023-11-22 18:15:43 -0500

    added vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Instrument_Sans } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 const instrumentSans = Instrument_Sans({ subsets: ["latin"] });
@@ -17,7 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={instrumentSans.className}>{children}</body>
+      <body className={instrumentSans.className}>
+        {children} <Analytics />
+      </body>
     </html>
   );
 }
